df215e93d18c236b81bf4004b816051baefbf1f9
MuhammadMaheem <muhammad.maheem@artificialautomationbizsol.com>
2025-08-29 09:05:35 +0500

    Updated Fixed

--- Lab Task 1/hungman.py
+++ Lab Task 1/hungman.py
@@ -64,14 +64,20 @@ def hangman():
     allowed_letters = ("abcdefghijklmnopqrstuvwxyz")
     guessing_word = random.choice(words).lower()
     guessed_letters = []
-    word_map= guessing(guessing_word)
+    word_map= guessing(guessing_word) 
+    # "programming" -> {'p': [0], 'r': [1, 4], 'o': [2], 'g': [3, 5], 'a': [6], 'm': [7, 8], 'i': [9], 'n': [10], 't': [11]}
     length_of_char = len(guessing_word)
+    # programming -> 11
+    displaying = replacing(word_map, guessed_letters, length_of_char)
+     # ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
     wrong_guesses = 0
     limit_guesses = len(Hangman) - 1
+    # limit_guesses = 7 (0-6)
     while True:
         letter = input("Enter a Letter to Guessing: ")
         if len(letter) != 1 or letter not in allowed_letters:
             print("Invaid character")
+            continue
         if letter in guessed_letters:
             print("You already guessed that")
             continue
@@ -80,6 +86,7 @@ def hangman():
             if letter in guessing_word :
                 guessed_letters.append(letter)
                 displaying = replacing(word_map, guessed_letters, length_of_char)
+                print(displaying)
                 if "_" not in displaying:
                     print("Big W!")
                     break
@@ -88,11 +95,14 @@ def hangman():
                 print(Hangman[wrong_guesses])
                 message = random.choice(messages)
                 print(message)
+                print(displaying)
                 if wrong_guesses == limit_guesses:
                     break
 
 def guessing(word):
     dic = {}
+    # dic = "programming" -> {'p': [0], 'r': [1, 4], 'o': [2], 'g': [3, 5], 'a': [6], 'm': [7, 8], 'i': [9], 'n': [10], 't': [11]}
+
     count = 0
     for char in word:
         if char not in dic:
@@ -105,13 +115,19 @@ def replacing(dictionary,guessed, lenght_of_letters):
   
 
     displaying = ["_"] * lenght_of_letters
+    # displaying = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
 
     for key, values in dictionary.items():
         if key in guessed:
             for pos in values:
                 displaying[pos] = key
-    print(displaying)
+                # displaying = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+                # displaying = ['p', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+
+
+
     return displaying
+    # displaying = ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
 
 hangman()
 
